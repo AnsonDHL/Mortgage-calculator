48c02ba550caabff3d6e2e668e7aa402476904d4
Anson <anson.liang@hotmail.com>
2019-08-26 17:47:32 -0600

    1st Version

---
+++ Assets/Scenes/MortgageCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MortgageCalculator : MonoBehaviour
+{
+
+    public TMP_InputField MortgageAmount;
+    public TMP_InputField InterestRate;
+    public TMP_InputField Amortization;
+    public TextMeshProUGUI MonthlyPayment;
+
+    public void CalculateMonthlyPayment()
+    {
+        float mortgagefloat = float.Parse(MortgageAmount.text);
+        float interestratefloat = float.Parse(InterestRate.text);
+        float amortizationfloat = float.Parse(Amortization.text);
+
+        double r = double.Parse(InterestRate.text) * 12 / 100;
+        double n = double.Parse(Amortization.text) * 12;
+
+        double MonthlyPaymentFloat = mortgagefloat * (1 + r) / n ;
+
+        MonthlyPayment.text = MonthlyPaymentFloat.ToString();
+
+
+    }
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
